76c4b3c69f0fca3acc38288536e46632219b9372
Etienne Garcia <etienne.garcia@etu.umontpellier.fr>
2022-11-23 11:17:44 +0100

    Nouvelle fonction js goNext pour passer à l'image suivante et modifier le html nécessaire y'a plus qu'à

--- main.js
+++ main.js
@@ -1,3 +1,28 @@
+const Image = document.getElementById("image").getAttribute("src");
+const INFO = document.getElementById("interface");
+const screen = document.getElementById("image");
+
+
 function goBack(){
     history.go(-1);
+}
+
+function goNext(){
+    if (Image == "Photos/FrontIUT.jpg"){
+        INFO.innerHTML = "<p>Bienvenue à l'IUT de Béziers j'espère que la visite vous plaira</p>\n" +
+            "    <p>Entrez vite n'ayez pas peur</p>\n" +
+            "    <style>\n" +
+            "      p {\n" +
+            "        position: center;\n" +
+            "        color: aqua;\n" +
+            "      }\n" +
+            "    </style>"
+        Image
+        console.log(INFO.innerHTML);
+        console.log(2);
+        
+    }
+    console.log(1);
+    console.log(Image);
+
 }
