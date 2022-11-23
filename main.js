c4b271e3a210a54d94198d5d84a3d5dbe587d637
Etienne Garcia <etienne.garcia@etu.umontpellier.fr>
2022-11-23 11:46:18 +0100

    La première page fonctionne, le boutton se déplace (gestion complète du CSS possible)
    Le contenue de #interface peut être modifier à volonté

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 const Image = document.getElementById("image").getAttribute("src");
 const INFO = document.getElementById("interface");
-const screen = document.getElementById("image");
+const SCREEN = document.getElementById("millieu");
 
 
 function goBack(){
@@ -17,11 +17,23 @@ function goNext(){
             "        color: aqua;\n" +
             "      }\n" +
             "    </style>"
-        Image
+        SCREEN.innerHTML= "<img src=\"Photos/hall-global.jpg\" id=\"image\">\n" +
+            "<button onclick=\"goNext()\" class=\"hide\" id=\"julien\"></button>\n" +
+            "<button onclick=\"goNext()\" class=\"hide\" id=\"nathan\"></button>\n" +
+            "<button onclick=\"goNext()\" class=\"hide\" id=\"fabio\"></button>\n" +
+            "<button onclick=\"goNext()\" class=\"hide\" id=\"etienne\"></button>\n" +
+            "<style>\n" +
+            "    #etienne {\n" +
+            "    position: absolute;\n" +
+            "    top: 30%;\n" +
+            "    left: 20%;\n" +
+            "    width: 100px;\n" +
+            "    height: 100px;\n" +
+            "}</style>"
         console.log(INFO.innerHTML);
-        console.log(2);
-        
+        console.log(2);        
     }
+    else if (Image == "Photos/hallglobal")
     console.log(1);
     console.log(Image);
 
